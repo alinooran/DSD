9161b0b0807e797651327db03aa0c52910d33978
ali nooran <angenesis04@gmail.com>
2021-12-12 18:16:32 +0330

    code formatted

--- Projects/HW7/P7.vhd
+++ Projects/HW7/P7.vhd
@@ -3,69 +3,67 @@ USE IEEE.std_logic_1164.ALL;
 USE IEEE.std_logic_unsigned.ALL;
 USE IEEE.std_logic_arith.ALL;
 
-ENTITY miniproc IS 
-    PORT (
-        clk, nrst   : IN    std_logic;
-        opcode      : IN    std_logic_vector(3 DOWNTO 0)
-    );
-END miniproc; 
+ENTITY miniproc IS
+	PORT (
+		clk, nrst : IN STD_LOGIC;
+		opcode : IN STD_LOGIC_VECTOR(3 DOWNTO 0)
+	);
+END miniproc;
 ARCHITECTURE behavioral OF miniproc IS
-    TYPE state IS (T0, T1, T2, T3, T4, T5);
+	TYPE state IS (T0, T1, T2, T3, T4, T5);
 	SIGNAL current_state, next_state : state;
 	-- Control signals
-	SIGNAL a_ld 	: std_logic;
-	SIGNAL b_ld 	: std_logic;
-	SIGNAL c_ld 	: std_logic;
-	SIGNAL d_ld 	: std_logic;
-	SIGNAL acc_ld 	: std_logic;
-	SIGNAL wr 	: std_logic;
-	SIGNAL cbus : std_logic_vector(31 DOWNTO 0);
-	SIGNAL sel 	: std_logic_vector(1 DOWNTO 0);
-	SIGNAL func : std_logic_vector(2 DOWNTO 0);
+	SIGNAL a_ld : STD_LOGIC;
+	SIGNAL b_ld : STD_LOGIC;
+	SIGNAL c_ld : STD_LOGIC;
+	SIGNAL d_ld : STD_LOGIC;
+	SIGNAL acc_ld : STD_LOGIC;
+	SIGNAL wr : STD_LOGIC;
+	SIGNAL cbus : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL sel : STD_LOGIC_VECTOR(1 DOWNTO 0);
+	SIGNAL func : STD_LOGIC_VECTOR(2 DOWNTO 0);
 
-    -- memory out
-    SIGNAL memOut;
-	
-    -- ALU out
-	SIGNAL Z 	: std_logic_vector(31 DOWNTO 0);
+	-- memory out
+	SIGNAL memOut;
+
+	-- ALU out
+	SIGNAL Z : STD_LOGIC_VECTOR(31 DOWNTO 0);
 
 	-- Registers
-	SIGNAL A 	: std_logic_vector(5 DOWNTO 0);
-	SIGNAL B 	: std_logic_vector(31 DOWNTO 0);
-	SIGNAL C 	: std_logic_vector(31 DOWNTO 0);
-	SIGNAL D 	: std_logic_vector(31 DOWNTO 0);
-	SIGNAL ACC 	: std_logic_vector(31 DOWNTO 0);
+	SIGNAL A : STD_LOGIC_VECTOR(5 DOWNTO 0);
+	SIGNAL B : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL C : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL D : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL ACC : STD_LOGIC_VECTOR(31 DOWNTO 0);
 
-    COMPONENT memory IS 
-        PORT (
-            clk     :    IN std_logic;
-            din     :    IN std_logic_vector(31 downto 0);
-            addr    :    IN std_logic_vector(5 downto 0);
-            wr      :    IN std_logic;
-            dout    :    OUT std_logic_vector(31 downto 0)
-        );
-    END COMPONENT;
+	COMPONENT memory IS
+		PORT (
+			clk : IN STD_LOGIC;
+			din : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			addr : IN STD_LOGIC_VECTOR(5 DOWNTO 0);
+			wr : IN STD_LOGIC;
+			dout : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+		);
+	END COMPONENT;
 
 BEGIN
 
-    -- memory instance
-    mem : memory PORT MAP(clk, cbus, A, wr, memOut);
+	-- memory instance
+	mem : memory PORT MAP(clk, cbus, A, wr, memOut);
 
-    -- bus mux
-	cbus <= B       WHEN sel = "00" ELSE
-			ACC     WHEN sel = "01" ELSE
-            memOut;
-			
-	z   <= 	C 		WHEN func = "000" ELSE
-			D 		WHEN func = "001" ELSE 
-			C + D   WHEN func = "010" ELSE
-			C AND D WHEN func = "011" ELSE
-			C XOR D WHEN func = "100" ELSE 
-			C - D   WHEN func = "101";
+	-- bus mux
+	cbus <= B WHEN sel = "00" ELSE
+		ACC WHEN sel = "01" ELSE
+		memOut;
 
-			
-    controlpath: PROCESS (current_state, opcode)
-    BEGIN
+	z <= C WHEN func = "000" ELSE
+		D WHEN func = "001" ELSE
+		C + D WHEN func = "010" ELSE
+		C AND D WHEN func = "011" ELSE
+		C XOR D WHEN func = "100" ELSE
+		C - D WHEN func = "101";
+	controlpath : PROCESS (current_state, opcode)
+	BEGIN
 		a_ld <= '0';
 		b_ld <= '0';
 		c_ld <= '0';
@@ -73,42 +71,72 @@ BEGIN
 		acc_ld <= '0';
 		func <= "000";
 		wr <= '0';
-		CASE current_state is
+		CASE current_state IS
 			WHEN T0 =>
 				CASE opcode IS
-					WHEN "0000"	  => func <= "000"; acc_ld <= '1'; next_state <= T0;
-					WHEN "0001"   => func <= "001"; acc_ld <= '1'; next_state <= T0;
-					WHEN "0010"   => func <= "010"; acc_ld <= '1'; next_state <= T0;  
-                    WHEN "0011"   => sel <= "00"; d_ld <= '1'; next_state <= T1;	
-					WHEN "0100"   => sel <= "00"; d_ld <= '1'; next_state <= T1;	
-					WHEN "0101"   => sel <= "10"; c_ld <= '1'; next_state <= T1;
-                    WHEN "0110"	  => sel <= "01"; wr <= '1'; next_state <= T0;
-					WHEN "0111"   => sel <= "00"; a_ld <= '1'; next_state <= T1;	
-					WHEN "1000"   => func <= "000"; acc_ld <= '1'; next_state <= T1;  
-                    WHEN "1001"   => sel <= "00"; d_ld <= '1'; next_state <= T1; 
-					WHEN OTHERS   => sel <= "00"; d_ld <= '1'; next_state <= T1;	
+					WHEN "0000" => func <= "000";
+						acc_ld <= '1';
+						next_state <= T0;
+					WHEN "0001" => func <= "001";
+						acc_ld <= '1';
+						next_state <= T0;
+					WHEN "0010" => func <= "010";
+						acc_ld <= '1';
+						next_state <= T0;
+					WHEN "0011" => sel <= "00";
+						d_ld <= '1';
+						next_state <= T1;
+					WHEN "0100" => sel <= "00";
+						d_ld <= '1';
+						next_state <= T1;
+					WHEN "0101" => sel <= "10";
+						c_ld <= '1';
+						next_state <= T1;
+					WHEN "0110" => sel <= "01";
+						wr <= '1';
+						next_state <= T0;
+					WHEN "0111" => sel <= "00";
+						a_ld <= '1';
+						next_state <= T1;
+					WHEN "1000" => func <= "000";
+						acc_ld <= '1';
+						next_state <= T1;
+					WHEN "1001" => sel <= "00";
+						d_ld <= '1';
+						next_state <= T1;
+					WHEN OTHERS => sel <= "00";
+						d_ld <= '1';
+						next_state <= T1;
 				END CASE;
 			WHEN T1 =>
 				CASE opcode IS
-					WHEN "01"   =>	sel <= "01"; y_ld <= '1'; nxt_state <= T2;
-					WHEN OTHERS =>	sel <= "01"; y_ld <= '1'; nxt_state <= T2;
+					WHEN "01" => sel <= "01";
+						y_ld <= '1';
+						nxt_state <= T2;
+					WHEN OTHERS => sel <= "01";
+						y_ld <= '1';
+						nxt_state <= T2;
 				END CASE;
 			WHEN T2 =>
 				CASE opcode IS
-					WHEN "01"   =>	func <= "00"; acc_ld <= '1'; nxt_state <= T0;
-					WHEN OTHERS =>	func <= "00"; acc_ld <= '1'; nxt_state <= T2;
+					WHEN "01" => func <= "00";
+						acc_ld <= '1';
+						nxt_state <= T0;
+					WHEN OTHERS => func <= "00";
+						acc_ld <= '1';
+						nxt_state <= T2;
 				END CASE;
 				nxt_state <= T3;
 			WHEN OTHERS =>
-				sel <= "10"; a_ld <= '1'; nxt_state <= T0;
+				sel <= "10";
+				a_ld <= '1';
+				nxt_state <= T0;
 				nxt_state <= T0;
 		END CASE;
-    END PROCESS controlpath;
-
-
-    datapath: PROCESS (clk)
-    BEGIN
-        IF clk = '1' THEN
+	END PROCESS controlpath;
+	datapath : PROCESS (clk)
+	BEGIN
+		IF clk = '1' THEN
 			IF nrst = '0' THEN
 				cur_state <= T0;
 				B <= X"00000001";
@@ -130,7 +158,7 @@ BEGIN
 				END IF;
 				cur_state <= nxt_state;
 			END IF;
-        END IF;
-    END PROCESS datapath;
+		END IF;
+	END PROCESS datapath;
 
 END behavioral;
