67897222141fcedb3b9e7bcdbce58f41c260ed63
Ali Nooran <angenesis04@gmail.com>
2022-01-27 14:06:36 +0330

    FL added

--- Projects/Final/FL.vhd
+++ Projects/Final/FL.vhd
@@ -4,33 +4,24 @@ USE ieee.std_logic_1164.ALL;
 ENTITY FL IS 
     PORT(
         inp     : IN std_logic_vector(31 downto 0);
-        --index   : IN std_logic;
-        KIi1    : IN std_logic_vector(15 downto 0);
-        KIi2    : IN std_logic_vector(15 downto 0);
-        KIi3    : IN std_logic_vector(15 downto 0);
+        KLi1    : IN std_logic_vector(15 downto 0);
+        KLi2    : IN std_logic_vector(15 downto 0);
         outp    : OUT std_logic_vector(31 downto 0)
     );
 END FL;
 ARCHITECTURE fl OF FL IS 
-    --COMPONENT shifter 
-    --    GENERIC(n : INTEGER := 16;
-    --            m : INTEGER := 1);
-    --    PORT(
-    --        a       : IN std_logic_vector(n-1 downto 0);
-    --        right   : IN std_logic;
-    --        y       : OUT std_logic_vector(n-1 downto 0)
-    --    );
-    --END COMPONENT;
-
-    SIGNAL l, r, a, b   : std_logic_vector(15 downto 0);
 BEGIN 
-    --shiftR  :   shifter GENERIC MAP () PORT MAP ();
-    --l <= inp(31 DOWNTO 16);
-    --r <= 
-    PROCESS (inp, index)
+    PROCESS (inp, KLi1, KLi2)
         VARIABLE l, r, a, b :   std_logic_vector(15 downto 0);
+        VARIABLE tmp        :   std_logic_vector(31 downto 0);
     BEGIN
         l := inp(31 DOWNTO 16);
         r := inp(15 DOWNTO 0);
+        a := l AND KLi1;
+        r := r XOR (a(14 DOWNTO 0) & a(15));
+        b := r OR KLi2;
+        l := l XOR (b(14 DOWNTO 0) & b(15));
+        tmp := l & r;
+        outp <= tmp;
     END PROCESS;
 END fl;
